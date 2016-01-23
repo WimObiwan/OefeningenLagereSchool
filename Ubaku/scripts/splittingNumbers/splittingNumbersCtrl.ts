76a7c15ac9d364c1b07f5020c211a6e35835cf3f
Jelle Druyts <jelle_druyts@yahoo.com>
2016-01-23 22:38:12 +0100

    Added support for restarting exercise

--- Ubaku/scripts/splittingNumbers/splittingNumbersCtrl.ts
+++ Ubaku/scripts/splittingNumbers/splittingNumbersCtrl.ts
@@ -2,7 +2,9 @@
     "use strict";
 
     interface ISplittingNumbersScope extends ng.IScope {
+        configuration: app.models.ExerciseConfiguration;
         exerciseDriver: app.models.IExerciseDriver;
+        startExercise(): void;
         respondToCurrentChallenge(answer: number): void;
         skipCurrentChallenge(): void;
     }
@@ -10,6 +12,19 @@
     class SplittingNumbersCtrl {
         public static $inject = ["$scope"];
         public constructor(private $scope: ISplittingNumbersScope) {
+            this.$scope.configuration = this.getDefaultConfiguration();
+            this.$scope.respondToCurrentChallenge = (answer) => this.$scope.exerciseDriver.respondToCurrentChallenge(answer);
+            this.$scope.skipCurrentChallenge = () => this.$scope.exerciseDriver.skipCurrentChallenge();
+            this.$scope.startExercise = () => this.startExercise();
+            this.startExercise();
+        }
+
+        public startExercise(): void {
+            this.$scope.exerciseDriver = new app.models.ExerciseDriver(this.$scope.configuration);
+            this.$scope.exerciseDriver.start();
+        }
+
+        private getDefaultConfiguration(): app.models.ExerciseConfiguration {
             var configuration = new app.models.ExerciseConfiguration();
 
             configuration.exerciseEndDriver.type = app.models.ExerciseEndDriverType.ChallengesCompleted;
@@ -29,10 +44,7 @@
 
             configuration.challengeEndDriver.type = app.models.ChallengeEndDriverType.Answered;
 
-            this.$scope.exerciseDriver = new app.models.ExerciseDriver(configuration);
-            this.$scope.exerciseDriver.start();
-            this.$scope.respondToCurrentChallenge = (answer) => this.$scope.exerciseDriver.respondToCurrentChallenge(answer);
-            this.$scope.skipCurrentChallenge = () => this.$scope.exerciseDriver.skipCurrentChallenge();
+            return configuration;
         }
     }
 
