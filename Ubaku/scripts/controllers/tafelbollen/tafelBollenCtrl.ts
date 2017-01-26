91b498b277c82337db83c61c910163374cb93fa7
Wim Devos <wim@obiwan.be>
2017-01-26 23:33:30 +0100

    Added initial tafelBollen logic (unfinished).

---
+++ Ubaku/scripts/controllers/tafelbollen/tafelBollenCtrl.ts
@@ -0,0 +1,71 @@
+﻿module app.tafelbollen {
+    "use strict";
+
+    interface ITafelBollenScope extends ng.IScope {
+        configuration: app.models.ExerciseConfiguration;
+        exerciseDriver: app.models.IExerciseDriver;
+        startExercise(): void;
+        stopExercise(): void;
+        moveBackward(): void;
+        moveForward(): void;
+        respondToCurrentChallenge(answer: number): void;
+        skipCurrentChallenge(): void;
+    }
+
+    class TafelBollenCtrl {
+        public static $inject = ["$scope", "$interval"];
+        public constructor(private $scope: ITafelBollenScope, private $interval: ng.IIntervalService) {
+            this.$scope.configuration = this.getDefaultConfiguration();
+            this.$scope.startExercise = () => this.startExercise();
+            this.$scope.stopExercise = () => this.stopExercise();
+            this.$scope.moveBackward = () => this.$scope.exerciseDriver.moveBackward();
+            this.$scope.moveForward = () => this.$scope.exerciseDriver.moveForward();
+            this.$scope.respondToCurrentChallenge = (answer) => this.$scope.exerciseDriver.respondToCurrentChallenge(answer);
+            this.$scope.skipCurrentChallenge = () => this.$scope.exerciseDriver.skipCurrentChallenge();
+            this.$scope.exerciseDriver = null;
+        }
+
+        public startExercise(): void {
+            var configuration = this.clone(this.$scope.configuration);
+            if (this.$scope.exerciseDriver !== null) {
+                this.$scope.exerciseDriver.stop();
+            }
+            this.$scope.exerciseDriver = new app.models.ExerciseDriver(
+                new app.models.ArithmeticChallengeFactory(configuration.arithmeticChallengeFactory),
+                configuration, this.$interval);
+            this.$scope.exerciseDriver.start();
+        }
+
+        public stopExercise(): void {
+            this.$scope.exerciseDriver.stop();
+            this.$scope.exerciseDriver = null;
+        }
+
+        private getDefaultConfiguration(): app.models.ExerciseConfiguration {
+            var configuration = new app.models.ExerciseConfiguration();
+
+            configuration.exerciseCompleteDriver.type = app.models.ExerciseCompleteDriverType.Infinite;
+            configuration.exerciseCompleteDriver.completeAfterChallengesCompleted = 10;
+            configuration.exerciseCompleteDriver.completeAfterChallengesSolved = 10;
+            configuration.exerciseCompleteDriver.completeAfterMinutes = 12;
+
+            configuration.arithmeticChallengeFactory.type = app.models.ChallengeFactoryType.Random;
+            configuration.arithmeticChallengeFactory.minNumber = 0;
+            configuration.arithmeticChallengeFactory.maxNumber = 10;
+            configuration.arithmeticChallengeFactory.primaryComponentSequence = app.models.SequenceType.Random;
+            configuration.arithmeticChallengeFactory.secondaryComponentSequence = app.models.SequenceType.Up;
+
+            configuration.challengeDriver.completeType = app.models.ChallengeCompleteType.Solved;
+            configuration.challengeDriver.endType = app.models.ChallengeEndType.ChallengeComplete;
+            configuration.challengeDriver.completeAfterSeconds = 10;
+
+            return configuration;
+        }
+
+        private clone<T>(value: T): T {
+            return JSON.parse(JSON.stringify(value));
+        }
+    }
+
+    angular.module(app.models.Constants.App.AngularAppName).controller(app.models.Constants.ControllerNames.TafelBollen, TafelBollenCtrl);
+}
